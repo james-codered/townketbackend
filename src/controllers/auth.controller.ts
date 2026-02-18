5b0f633c930ae6d90b7ac5dd69e44440c2e3580a
james-codered <jamessamuelsamuel4@gmail.com>
2026-02-18 23:46:11 +0100

    Update auth.controller.ts

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from "express";
+import crypto from "crypto";
+import User from "../models/User";
 import bcrypt from "bcryptjs";
 import User from "../models/User";
 import generateToken from "../utils/generateToken";
@@ -16,6 +18,35 @@ export const forgotPassword = async (req: Request, res: Response) => {
   // Later we can implement reset token + email service
 
   res.json({ message: "Password reset link sent (demo)" });
+};
+export const forgotPassword = async (req: any, res: any) => {
+  try {
+    const { email } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(200).json({
+        message: "If this email exists, a reset link has been sent.",
+      });
+    }
+
+    // Generate reset token
+    const resetToken = crypto.randomBytes(20).toString("hex");
+
+    user.resetPasswordToken = resetToken;
+    user.resetPasswordExpires = new Date(Date.now() + 10 * 60 * 1000); // 10 minutes
+
+    await user.save();
+
+    // For now we just return the token (later we send email)
+    res.status(200).json({
+      message: "Reset token generated",
+      resetToken,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
 };
 
 export const register = async (req: Request, res: Response) => {
