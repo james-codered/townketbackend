a52ae763800358060a595d3d3bc604ac3e6a989c
james-codered <jamessamuelsamuel4@gmail.com>
2026-02-18 23:56:49 +0100

    Update auth.routes.ts

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/reset-password/:token", resetPassword);
 router.post("/forgot-password", forgotPassword);
 
 export default router;
