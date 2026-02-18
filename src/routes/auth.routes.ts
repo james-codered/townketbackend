b0e5f60f608dd2332f16504a61f6946274f8ae5a
james-codered <jamessamuelsamuel4@gmail.com>
2026-02-19 00:12:33 +0100

    Update auth.routes.ts

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,11 +1,16 @@
 import express from "express";
-import { register, login, forgotPassword } from "../controllers/auth.controller";
+import {
+  register,
+  login,
+  forgotPassword,
+  resetPassword,
+} from "../controllers/auth.controller";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.post("/reset-password/:token", resetPassword);
 router.post("/forgot-password", forgotPassword);
+router.post("/reset-password/:token", resetPassword);
 
 export default router;
